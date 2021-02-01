1dc96a2dc42fba47c654ca241dc2acfc025131c9
Frank Bodholdt Jakobsen <au237297@uni.au.dk>
2021-02-01 08:13:42 +0100

    Add project files.

---
+++ CashRegister.Test.Unit/UnitTest1.cs
@@ -0,0 +1,59 @@
+using System;
+using NUnit.Framework;
+
+namespace CashRegister.Test.Unit
+{
+    [TestFixture]
+    public class RegisterTests
+    {
+        private Register uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Register();
+        }
+
+        [Test]
+        public void AddItem_OneItem_NoOfItemsIs1()
+        {
+            // Arrange
+
+            // Act
+            uut.AddItem(12.50);
+
+            // Assert
+            Assert.That(uut.GetNoOfItems(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddItem_OneItem_TotalIsCorrect()
+        {
+            // Arrange
+
+            // Act
+            uut.AddItem(13.45);
+
+            // Assert
+            Assert.That(uut.GetTotal(), Is.EqualTo(13.45));
+        }
+
+        [Test]
+        public void ctor_NoItemsAdded_NoOfItemsIsZero()
+        {
+            // Arrange er i SetUp
+            // Act - der skal ikke være nogen act
+            // Assert
+            Assert.That(uut.NoOfItems, Is.Zero);
+        }
+
+
+
+        [Test]
+        public void AddItem_NegativeItem_Throws()
+        {
+            // Act + Assert
+            Assert.That(() => uut.AddItem(-10), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+    }
+}
---
+++ CashRegister/Register.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashRegister
+{
+    public class Register
+    {
+        public int NoOfItems { get; set; }
+        private double _total;
+        public Register()
+        {
+            _total = 0.0;
+            NoOfItems = 0;
+        }
+
+        public void AddItem(double itemPrice)
+        {
+            if (itemPrice < 0)
+                throw new ArgumentOutOfRangeException("Prisen er mindre end nul");
+
+            _total += itemPrice;
+            NoOfItems++;
+        }
+
+        public double GetTotal()
+        {
+            return _total;
+        }
+
+        public int GetNoOfItems()
+        {
+            return NoOfItems;
+        }
+
+    }
+}
