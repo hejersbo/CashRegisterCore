d060608faac57e17a529e20474d0f536f91e4915
Henrik Ejersbo <hejersbo@ece.au.dk>
2021-11-09 09:40:04 +0100

    Remove AddName

--- CashRegister/Register.cs
+++ CashRegister/Register.cs
@@ -17,11 +17,6 @@ namespace CashRegister
            itemList = new List<double>(10);
         }
 
-        public void AddName(string name)
-        {
-	    Console.Writeline("AddName");
-            _name = name?.Trim();
-        }
 
         public string GetName()
         {
